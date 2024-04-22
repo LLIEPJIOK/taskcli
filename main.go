f33486b284b342bf81e23dfdf60255c9a0bb5e90
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-04-22 23:21:48 +0300

    calendar begining

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 	"taskcli/cmds"
 	"taskcli/database"
+	"taskcli/task"
 )
 
 func main() {
 	defer database.CloseDB()
+	task.PrintCells()
 	if err := cmds.RootCommand.Execute(); err != nil {
 		log.Fatal("cannot execute root command:", err)
 	}
